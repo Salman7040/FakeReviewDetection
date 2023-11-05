00db77deb8af67176b8df53dbface3c4db2663bb
salman <hashmisalman7040@gmail.com>
2023-11-05 17:55:59 +0530

    php include

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$Ip=$_SERVER["REMOTE_ADDR"];
+$userReq=$_GET["t1"];
+$var='';    
+if($Ip==$var)
+echo"its repeated";
+else
+$var=$Ip;
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ search.js
@@ -0,0 +1,28 @@
+var a=document.querySelectorAll("h1")
+
+var searchVar=document.getElementById("search-bar")
+var main=document.getElementById("main-page")
+var phone=document.getElementsByClassName("phone")
+
+
+console.log(phone[1].innerHTML)
+function searchNow(){
+
+    let i=0;
+a.forEach(function(e){
+    
+    if(e.innerText.toLowerCase()==searchVar.value.toLowerCase()){       
+        
+        
+    }
+    else{
+        
+    }
+    
+
+})
+}
+
+
+
+
